d1a42e806959354f9d796c2fd0a576047a74fb8b
Xen | Braxton <55970625+braxtongpoll@users.noreply.github.com>
2021-06-11 20:10:10 -0600

    Update prefix.js

--- commands/Configuration/prefix.js
+++ commands/Configuration/prefix.js
@@ -2,11 +2,13 @@ const { MessageEmbed } = require(`discord.js`);
 const fs = require(`fs`);
 const path = require("path");
 exports.run = async(client, message, args) => {
-
+    let prefix = args.join(" ");
+    if (!prefix) return client.ia(message, exports.info.arguments);
+    client.docUpdate(message, "prefix", prefix, `Your prefix was changed to **${prefix}**`)
 }, exports.info = {
     name: "prefix",
     aliases: [],
     permission: `ADMINISTRATOR`,
     description: `Change the prefix of the bot.`,
-    arguments: "N/A"
+    arguments: "NEW_PREFIX"
 }
